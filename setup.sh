0b9ff8468e69f52ac87fbab3eacaec9a84933e81
disciplae1 <79573652+disciplae1@users.noreply.github.com>
2021-03-30 07:38:11 -0300

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,11 +1,7 @@
 mkdir -p ~/.sample_predict/
-echo "\
-[general]\n\
-email = \"omena.lae1@gmail.com\"\n\
-" > ~/.sample_predict/credentials.tom1
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.sample_predict/config.tom1
+
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.sample_predict/config.toml
